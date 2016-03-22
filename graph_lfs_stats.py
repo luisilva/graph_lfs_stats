59bf6f6c4a0420d49be7d1996550d88ab86e0ab2
Luis Silva <luis_silva@harvard.edu>
2016-03-22 09:12:24 -0400

    removing final print statement and moving it into logging.

--- graph_lfs_stats.py
+++ graph_lfs_stats.py
@@ -212,6 +212,7 @@ class lfs_stats:
 
   def push_to_graphite(self):
     print self.delta_data
+    logger.debug("MDS delta data output: \n %s" self.delta_data)
     metrics = self.delta_data
     #metrics = json.loads(self.jdata)
     content = []
